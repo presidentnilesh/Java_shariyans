558e36436799431cfe93601ca8fd7e63f1e98c33
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-15 22:28:51 +0530

    methods

---
+++ basic/first.java
@@ -0,0 +1,12 @@
+public class first {
+    public static void nilu(){
+        int a=10 , b=23 ;
+        int sum=a+b ;
+        System.out.println("The sun of a and b is :"+ sum);
+    }
+    
+    public static void main (String[] args ){
+        nilu();
+        
+    }
+}
