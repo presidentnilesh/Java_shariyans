39ef7c691441b4d5df8045a162893b326171be03
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-15 12:51:11 +0530

    MIRRORED RIGHT TRIANGLE

--- nasted.java
+++ nasted.java
@@ -9,15 +9,71 @@
 //     }
 // }
 
+        // PYRAMID
+// public class nasted {
+//     public static void main(String[] args) {
+
+//         int n = 6;
+
+//         for (int i = 0; i < n; i++) {
+
+//             // spaces
+//             for (int j = 0; j < n - i - 1; j++) {
+//                 System.out.print("  ");
+//             }
+
+//             // stars
+//             for (int j = 0; j < 2 * i + 1; j++) {
+//                 System.out.print("* ");
+//             }
+
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+        // RIGHT ANGLED TRIANGLE 
+
+// public class nasted {
+//     public static void main (String[] args ){
+//         for ( int i= 0 ; i<6 ;i++ ){
+//             for ( int j=0 ; j<=i ; j++ ){
+//                 System.out.print("* ");
+//             }
+//             System.out.println("");
+//         }
+//     }
+// }
+
+        // INVERTED RIGHT TRAINGLE
+
+// public class nasted {
+// public static void main(String[] args){
+//     for ( int i = 0 ; i <6 ; i++){
+//         for ( int j = 0 ; j<(6-i) ; j++){
+//             System.out.print("* ");
+//         }
+//         System.out.println(" ");
+//     }
+// }
+// }
+
+        // MIRRORED RIGHT TRIANGLE
 
 public class nasted {
     public static void main(String[] args ){
-        int n = 6 ;
-         for ( int i=0 ; i<=n ; i++){
-            for ( int j=(n/2)-1 ; j<=(n/2)+i ;j++){
-                System.out.print("* ");
+        for ( int i= 0 ; i<6 ; i++){
+            for ( int j=0 ; j<( 6-i) ; j++){
+                System.out.print(" ");
             }
-            System.out.println("");
+            for ( int j = (6-i); j<6 ; j++){
+
+                System.out.print("*");
+            }
+            System.out.println(" ");
+            
         }
     }
 }
+
