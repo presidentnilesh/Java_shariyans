3cb8e78c34799f56babbb02cce4e7537405fb93e
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-17 08:12:11 +0530

    learning aarray all application and logic

---
+++ basic/summ.java
@@ -0,0 +1,44 @@
+// import java.util.Scanner;
+
+// public class arr {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the size of array. ");
+//         int size = sc.nextInt();
+//         int arr[] = new int[size] ;
+//         System.out.println("Enter the "+ size + "elements of the array .");
+        
+//         for ( int i=0 ; i<arr.length ; i++){
+//             arr[i] = sc.nextInt() ;
+//         }
+//         for ( int i=0 ; i<arr.length ; i++ ){
+//             System.out.println(i + " element of array is : " + arr[i]);
+//         }
+//     }
+// }
+
+                //sum of array 
+
+// public class summ{
+//         public static void main(String[] args){
+//                 int sum = 0 ;
+//                 int arr[] = { 10 , 20 , 30 , 40} ;
+//                 for ( int i=0 ; i<arr.length ; i++ ){
+//                         sum += arr[i] ;
+//                 }
+//                 System.out.println(sum);
+//         }
+// }
+
+
+                // find max from the list of array
+
+public class summ{
+        public static void main(String[] args){
+                int arr[] = {10 , 3 , 7, 11 , 10 , 4 };
+                int max=arr[0];
+                for ( int i=0 ; i<arr.length ; i++){
+                        
+                }
+        }
+}
