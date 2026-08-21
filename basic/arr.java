7c31b23580e6167f117bd0e308007f78e88991a6
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-21 11:42:40 +0530

    shorting

--- basic/arr.java
+++ basic/arr.java
@@ -313,12 +313,11 @@ public class arr{
         public static void main (String[] args){
                 int arr[] = { 0 ,5 ,4,7,3,3 ,2,1};
                 for ( int i = 0 ; i<arr.length ; i++){
-                        for ( int j = i+1 ; j<arr.length -1 ; j++){
+                        for ( int j = i+1 ; j<arr.length  ; j++){
                                 if ( arr[i] > arr[j]){
-                                        int temp = arr[j];
+                                        int temp = arr[i];
                                         arr[i]=arr[j];
                                         arr[j]= temp ;
-                                        i++ ;
                                 }
                         }
                 }
