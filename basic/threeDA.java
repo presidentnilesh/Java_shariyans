7caa47d0bceb3d4f4b85118217146459ccb35da2
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-23 09:03:52 +0530

    jagged array

--- basic/threeDA.java
+++ basic/threeDA.java
@@ -14,26 +14,51 @@
 
 
         // another method , to take arry from the user only and then printing .
+// import java.util.Arrays;
+// import java.util.Scanner ;
+// public class threeDA{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the number of rows : ");
+//         int r = sc.nextInt();
+//         System.out.println("Enter the number of column : ");
+//         int c = sc.nextInt();
+//         int arr[][]= new int[r][c];
+
+//         for(int i =0 ; i<r ; i++){
+//             for(int j=0 ; j<c ; j++){
+//                 System.out.println("Enter the arr : ["+i+"]["+j+"]");
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+//          for(int i =0 ; i<r ; i++){
+//                 System.out.print(Arrays.toString(arr[i]) + " ");
+//                 System.out.println();
+//             }
+//         }
+// }
+
+        // jagged array 
+
 import java.util.Arrays;
 import java.util.Scanner ;
 public class threeDA{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows : ");
-        int r = sc.nextInt();
-        System.out.println("Enter the number of column : ");
-        int c = sc.nextInt();
-        int arr[][]= new int[r][c];
+        int arr[][]= {{1,2,3,4},{5,6,7},{8,9}};
 
-        for(int i =0 ; i<r ; i++){
-            for(int j=0 ; j<c ; j++){
-                System.out.println("Enter the arr : ["+i+"]["+j+"]");
-                arr[i][j] = sc.nextInt();
+        for(int i =0 ; i<arr.length ; i++){
+            for(int j=0 ; j<arr[i].length ; j++){
+                System.out.println("enter the matrix for ["+i+"] ["+j+"]");
+                arr[i][j]= sc.nextInt();
             }
         }
-         for(int i =0 ; i<r ; i++){
-                System.out.print(Arrays.toString(arr[i]) + " ");
-                System.out.println();
+        for(int i =0 ; i<arr.length ; i++){
+            for(int j=0 ; j<arr[i].length ; j++){
+                System.out.print(arr[i][j] + " ");
             }
+            System.out.println();
+        }
+        
         }
 }
