e3a134bf04d08fde2ff0e606a5c02ce6d293a0e9
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-23 17:04:06 +0530

    diamond

--- basic/pattern.java
+++ basic/pattern.java
@@ -1,10 +1,28 @@
-public class pattern {
-    public static void main(String[] args) {
-        int n = 9 ;
-        for ( int r=1 ; r<=2*n-1 ; r++){
-            
-        }
+        //pyramid question
 
-    }
+// public class pattern {
+//     public static void main(String[] args) {
+//         int n = 9 ;
+//         int Totalcol , c  ;
+//         for ( int r=1 ; r<=2*n-1 ; r++){
+            
+//             if ( r<=n){
+//                 Totalcol = r ;
+//             } else{
+//                 Totalcol = 2*n-r ;
+//             }
+//             for (int s=1 ; s<=n-Totalcol ; s++){
+//                 System.out.print(" ");
+//             }
+//             for(int p =1 ; p<=Totalcol ; p++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
     
-}
+// }
+
+
+            // LEFT BUTTERFLY 
+
--- basic/tempCodeRunnerFile.java
+++ basic/tempCodeRunnerFile.java
@@ -1,5 +1,2 @@
- // else{
-                        //         for( int s=1 ; s<n ;s++ ){
-                        //                 System.out.print("* ");
-                        //         }
-                        // }
+
+        System.out.println();
