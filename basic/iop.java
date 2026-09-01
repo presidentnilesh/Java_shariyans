3250255e73d3a9e1ed3bb47c32b29cfc05a9d778
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-09-01 10:15:10 +0530

    iop exam preperation

---
+++ basic/iop.java
@@ -0,0 +1,50 @@
+    // pyramid 
+// import java.util.Scanner;
+// public class iop {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the value of n :");
+//         int n = sc.nextInt();
+//         for(int i = 0 ; i<n ;i++){
+//             for( int j =0 ; j<n-i-1 ; j++){
+//                 System.out.print(" ");
+//             }
+//             for (int j=0 ; j<i+1 ; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//         }
+// }
+
+
+// Right triagle 
+// public class iop {
+//     public static void main(String[] args){
+//         int n = 6 ;
+//         for (int i=0 ; i<n ; i++){
+//             for( int j =0 ; j<=i ; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Left triangle 
+// public class iop {
+//     public static void main(String[] args){
+//         int n = 6 ;
+//         for (int i=0 ; i<n ; i++){
+//             for( int j =0 ; j<n-i ; j++){
+//                 System.out.print(" ");
+//             }
+//             for( int j =0 ;  j<=i; j++ ){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// 
