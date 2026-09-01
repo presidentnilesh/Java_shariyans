4284b26bb4d653a2ba4dbc0890f83a1a66b76bc5
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-09-01 19:49:56 +0530

    exam prep

--- basic/iop.java
+++ basic/iop.java
@@ -47,4 +47,64 @@
 //     }
 // }
 
-// 
+// diamond 
+// public class iop {
+//     public static void main(String[] args){
+//         int n = 10 ;
+//         for ( int i =0 ; i<(n)  ; i++){
+//             for( int j =0 ; j<n-i ; j++){
+//                 System.out.print(" ");
+//             }
+//             for( int j =0 ; j<=i ; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//         for ( int i =0 ; i<(n)  ; i++){
+//             for( int j =0 ; j<=i ; j++){
+//                 System.out.print(" ");
+//             }
+//             for( int j =0 ; j<(n/2)-i ; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+public class iop {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper half
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
